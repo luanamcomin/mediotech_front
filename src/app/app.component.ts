ccf24eca246c501c2e650c803aae63592d865776
luanamcomin <luana.martinscomin@gmail.com>
2024-09-22 21:17:22 -0300

    routs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { RouterOutlet, Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    NavbarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app_mediotech';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SubjectsComponent } from './pages/subjects/subjects.component';
+import { ClassesComponent } from './pages/classes/classes.component';
+import { UsersComponent } from './pages/users/users.component';
+import { AnnoucementsComponent } from './pages/annoucements/annoucements.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'subjects', component: SubjectsComponent},
+  { path: 'classes', component: ClassesComponent},
+  { path: 'users', component: UsersComponent},
+  { path: 'annoucements', component: AnnoucementsComponent},
+
+];
